c5e1208b151566d74554781c194f45455dec7c5d
2333xiang <odokoei@outlook.com>
2025-03-06 19:10:27 +0800

    实现添加和移除音频轨道

--- audiomanager.h
+++ audiomanager.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <memory>
+#include <unordered_map>
 
 #include "audioorbitcontroller.h"
 
@@ -23,9 +24,14 @@ class AudioManager : public QWidget {
  private:
   Ui::AudioManager *ui;
   // 此设备的音频管理器所保存的所有音频轨道
-  std::vector<AudioOrbitController> audio_orbits;
+  std::unordered_map<std::shared_ptr<XAudioOrbit>, AudioOrbitController *>
+      audio_orbits;
   // 此设备的音频管理器所对应的输出设备
   std::shared_ptr<XOutputDevice> outdevice;
+  // 添加音频轨道
+  void add_audio_orbit(std::shared_ptr<XAudioOrbit> &audioorbit);
+  // 移除音频轨道
+  void remove_audio_orbit(std::shared_ptr<XAudioOrbit> &audioorbit);
 };
 
 #endif  // AUDIOMANAGER_H
